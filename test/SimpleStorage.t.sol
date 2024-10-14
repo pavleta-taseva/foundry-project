89c417b5fa77cf3405939ac228aa1903b3b3036a
kumiho <pavleta.taseva@gmail.com>
2024-10-14 18:05:36 +0000

    Add test file

---
+++ test/SimpleStorage.t.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.28;
+
+import {Test} from "forge-std/Test.sol";
+import {DeploySimpleStorage} from "../script/DeploySimpleStorage.s.sol";
+import {SimpleStorage} from "../src/SimpleStorage.sol";
+
+contract SimpleStorageTest is Test {
+    
+}
+
