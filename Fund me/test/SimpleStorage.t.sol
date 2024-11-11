f890a1d55bd56386892e9d6308df1b3153f5f8d9
kumiho <pavleta.taseva@gmail.com>
2024-11-11 11:34:03 +0000

    chore: forge init

--- Fund me/test/SimpleStorage.t.sol
+++
@@ -1,9 +0,0 @@
-//SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.26;
-
-import {Test} from "forge-std/Test.sol";
-import {DeploySimpleStorage} from "../script/DeploySimpleStorage.s.sol";
-import {SimpleStorage} from "../src/SimpleStorage.sol";
-
-contract SimpleStorageTest is Test {}
---
+++ test/FundmeTest.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.26;
+
+import { Test } from "forge-std/Test.sol";
+
+contract FundMeTest {
+    
+}
